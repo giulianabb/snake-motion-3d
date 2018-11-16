81296f9a184ae0336e84781c3bf493e15a9d98f0
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2018-11-16 21:32:23 -0200

    Directions code, first version

--- snake_motion/game/snake.py
+++ snake_motion/game/snake.py
@@ -9,11 +9,11 @@ class Direction(Enum):
 
 class Face(IntEnum):
     FRONT = 0
-    RIGHT = 1
+    TOP = 1
     BACK = 2
-    LEFT = 3
-    TOP = 4
-    DOWN = 5
+    DOWN = 3
+    LEFT = 4
+    RIGHT = 5
 
 class SnakePart:
 
@@ -52,11 +52,132 @@ class Snake(SnakePart):
         super().__init__(2, 2, 0)
         __nextPart = SnakePart(2, 3, 0)
 
+
     def update_direction(direction):
         """
         Atualiza a direção
         """
         self.__direction = direction
+
+
+    def move_to_direction():
+        if self.__direction == Direction.NORTH:
+            self.__lin += 1
+        elif self.direction == Direction.EAST:
+            self.__col -= 1
+        elif self.direction == Direction.WEST:
+            self.__col += 1
+        elif self.direction == Direction.SOUTH:
+            self.__lin -= 1
+
+
+    def move_to_upper_cube():
+
+        # Column keeps adding and rotating
+        if self.__face < Face.DOWN:
+            self.__face += 1
+            self.__col = 0
+        elif self.__face == Face.DOWN:
+            self.__face = Face.FRONT
+            self.__col = 0
+
+        # Column swaps with line, and now line will add
+        elif self.__face == Face.LEFT:
+            self.__face = Face.FRONT
+            self.__direction = Direction.WEST
+            self.__col = self.__lin
+            self.__lin = 0
+        elif self.__face == Face.RIGHT:
+            self.__face = Face.BACK
+            self.__direction = Direction.WEST
+            self.__col = self.__lin
+            self.__lin = 0
+
+
+    def move_to_lower_cube():
+
+        # Column swaps with line, and now line will subtract
+        if self.__face == Face.RIGHT:
+            self.__face = Face.FRONT
+            self.__direction = Face.EAST
+            self.__col = 3 - self.__lin
+            self.__lin = 3
+        elif self.__face == Face.LEFT:
+            self.__face = Face.BACK
+            self.__direction = Face.EAST
+            self.__col = 3 - self.__lin
+            self.__lin = 3
+
+        # Column keeps subtracting and rotating
+        elif self.__face == Face.FRONT:
+            self.__face = Face.DOWN
+            self.__col = 3
+        elif self.__face > Face.FRONT:
+            self.__face -= 1
+            self.__col = 3
+
+
+    def move_to_left_cube():
+
+        if self.__face == Face.FRONT:
+            self.__face = Face.LEFT
+            self.__direction = Direction.SOUTH
+            self.__lin = self.__col
+            self.__col = 3
+        elif self.__face == Face.TOP:
+            self.__face = Face.LEFT
+            # self.__direction = Direction.EAST (redundant)
+            self.__col = 3 - self.__col
+            self.__lin = 3
+        elif self.__face == Face.BACK:
+            self.__face = Face.LEFT
+            self.__direction = Direction.NORTH
+            self.__lin = 3 - self.__col
+            self.__col = 0
+        elif self.__face == Face.DOWN:
+            self.__face = Face.LEFT
+            self.__direction = Direction.WEST
+            self.__lin = 0
+        elif self.__face == Face.LEFT:
+            self.__face = Face.DOWN
+            self.__direction = Direction.WEST
+            self.__lin = 0
+        elif self.__face == Face.RIGHT:
+            self.__face = Face.TOP
+            # self.__direction = Direction.EAST (redundant)
+            self.__col = 3 - self.__col
+            self.__lin = 3
+
+    def move_to_right_cube():
+
+        if self.__face == Face.FRONT:
+            self.__face = Face.RIGHT
+            self.__direction = Direction.SOUTH
+            self.__lin = 3 - self.__col
+            self.__col = 3
+        elif self.__face == Face.TOP:
+            self.__face = Face.RIGHT
+            # self.__direction = Direction.WEST (redundant)
+            self.__col = 3 - self.__col
+            self.__lin = 0
+        elif self.__face == Face.BACK:
+            self.__face = Face.RIGHT
+            self.__direction = Direction.NORTH
+            self.__lin = self.__col
+            self.__col = 0
+        elif self.__face == Face.DOWN:
+            self.__face = Face.RIGHT
+            self.__direction = Direction.EAST
+            self.__lin = 3
+        elif self.__face == Face.RIGHT:
+            self.__face = Face.DOWN
+            self.__direction = Direction.EAST
+            self.__lin = 3
+        elif self.__face == Face.LEFT:
+            self.__face = Face.TOP
+            # self.__direction = Direction.WEST (redundant)
+            self.__col = 3 - self.__col
+            self.__lin = 0
 
     def update_position(add_tail):
         """
@@ -66,7 +187,16 @@ class Snake(SnakePart):
         if self.__nextPart != None:
             self.__nextPart.update_position(self.__lin, self.__col, self.__face, add_tail)
         else:
-            
+            move_to_direction()
+            if self.__col > 3:
+                move_to_upper_cube()
+            if self.__col < 0:
+                move_to_lower_cube()
+            if self.__lin < 0:
+                move_to_left_cube()
+            if self.__lin > 3:
+                move_to_right_cube()
+
 
     def get_head_position():
         return (self.__lin, self.__col, self.__face)
