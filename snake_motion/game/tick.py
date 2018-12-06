1c38913d97592e2f6014507b29b96c1502222d3b
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2018-12-06 18:19:44 -0200

    compile

--- snake_motion/game/tick.py
+++ snake_motion/game/tick.py
@@ -6,6 +6,11 @@ class Tick:
     __tick_count = 0
     __ms_per_tick = 500
 
+    def elapsed_ms(self, last_time):
+        dt = datetime.now() - last_time
+        ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+        return ms
+
     def __init__(self, on_tick_callback):
         self.__onTick = on_tick_callback
         last_time = datetime.now()
@@ -15,11 +20,6 @@ class Tick:
                 self.__tick_count = (self.__tick_count + 1) % 4
                 self.__on_tick(self.__tick_count)
 
-    def elapsed_ms(last_time):
-        dt = datetime.now() - last_time
-        ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-        return ms
-
-    def increase_velocity():
+    def increase_velocity(self):
         if self.__ms_per_tick > 100:
             self.__ms_per_tick -= 5
