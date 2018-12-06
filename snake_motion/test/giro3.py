8ae995da3203ee963828cff600c7aee6c693c03c
Giuliana Barone <giulianabb@gmail.com>
2018-12-06 18:03:41 -0200

    teste giro

--- snake_motion/test/giro3.py
+++ snake_motion/test/giro3.py
@@ -127,7 +127,8 @@ def Print(Str):
   l=len(Str)
   for i in range(l):
     write(ord(Str[i]))
-   
+
+
 def setCursor(x,y):
   if y == 0:
     n=128+x
@@ -135,13 +136,14 @@ def setCursor(x,y):
     n=192+x
   cmd(n)
 
+
 def InitMPU():
-bus.write_byte_data(Device_Address, DIV, 7)
-bus.write_byte_data(Device_Address, PWR_M, 1)
-bus.write_byte_data(Device_Address, CONFIG, 0)
-bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-bus.write_byte_data(Device_Address, INT_EN, 1)
-time.sleep(1)
+  bus.write_byte_data(Device_Address, DIV, 7)
+  bus.write_byte_data(Device_Address, PWR_M, 1)
+  bus.write_byte_data(Device_Address, CONFIG, 0)
+  bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+  bus.write_byte_data(Device_Address, INT_EN, 1)
+  time.sleep(1)
 
 def display(x,y,z):
   x=x*100
@@ -162,29 +164,30 @@ def display(x,y,z):
   Print(str(z))
   Print("   ")
 
-  print x
-  print y
-  print z
+  print(x)
+  print(y)
+  print(z)
 
 def readMPU(addr):
-high = bus.read_byte_data(Device_Address, addr)
-low = bus.read_byte_data(Device_Address, addr+1)
-value = ((high << 8) | low)
-if(value > 32768):
-value = value - 65536
-return value
+  high = bus.read_byte_data(Device_Address, addr)
+  low = bus.read_byte_data(Device_Address, addr + 1)
+  value = ((high << 8) | low)
+  if(value > 32768):
+    value = value - 65536
+  return value
+
 def accel():
-x = readMPU(ACCEL_X)
-y = readMPU(ACCEL_Y)
-z = readMPU(ACCEL_Z)
+  x = readMPU(ACCEL_X)
+  y = readMPU(ACCEL_Y)
+  z = readMPU(ACCEL_Z)
 
-Ax = (x/16384.0-AxCal)
-Ay = (y/16384.0-AyCal)
-Az = (z/16384.0-AzCal)
+  Ax = (x/16384.0-AxCal)
+  Ay = (y/16384.0-AyCal)
+  Az = (z/16384.0-AzCal)
 
-#print "X="+str(Ax)
-display(Ax,Ay,Az)
-time.sleep(.01)
+  #print("X="+str(Ax))
+  display(Ax,Ay,Az)
+  time.sleep(.01)
 
 def gyro():
   global GxCal
@@ -196,7 +199,7 @@ def gyro():
   Gx = x/131.0 - GxCal
   Gy = y/131.0 - GyCal
   Gz = z/131.0 - GzCal
-  #print "X="+str(Gx)
+  #print("X="+str(Gx))
   display(Gx,Gy,Gz)
   time.sleep(.01)
 
@@ -204,7 +207,7 @@ def temp():
   tempRow=readMPU(TEMP)
   tempC=(tempRow / 340.0) + 36.53
   tempC="%.2f" %tempC
-  print tempC
+  print(tempC)
   setCursor(0,0)
   Print("Temp: ")
   Print(str(tempC))
@@ -230,9 +233,9 @@ def calibrate():
   AyCal = y/16384.0
   AzCal = z/16384.0
  
-  print AxCal
-  print AyCal
-  print AzCal
+  print(AxCal)
+  print(AyCal)
+  print(AzCal)
 
   global GxCal
   global GyCal
@@ -251,9 +254,9 @@ def calibrate():
   GyCal = y/131.0
   GzCal = z/131.0
 
-  print GxCal
-  print GyCal
-  print GzCal
+  print(GxCal)
+  print(GyCal)
+  print(GzCal)
 
 begin();
 Print("MPU6050 Interface")
@@ -277,4 +280,3 @@ while 1:
   time.sleep(1)
   for i in range(30):
     gyro()
-     
