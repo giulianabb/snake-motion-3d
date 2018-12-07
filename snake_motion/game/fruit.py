d8d2328d29a2c123800b8bc97315b221b115fb33
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2018-12-07 11:54:12 -0200

    test

--- snake_motion/game/fruit.py
+++ snake_motion/game/fruit.py
@@ -2,14 +2,14 @@ import random
 
 class Fruit:
 
-    __lin = 0
-    __col = 0
-    __face = 0
+    lin = 0
+    col = 0
+    face = 0
 
     def __init__(self):
-        self.__lin = random.randint(0, 3)
-        self.__col = random.randint(0, 3)
-        self.__face = random.randint(0, 5)
+        self.lin = random.randint(0, 3)
+        self.col = random.randint(0, 3)
+        self.face = random.randint(0, 5)
 
     def has_been_eaten(self, slin, scol, sface):
-        return slin == self.__lin and scol == self.__col and sface == self.__face
+        return slin == self.lin and scol == self.col and sface == self.face
