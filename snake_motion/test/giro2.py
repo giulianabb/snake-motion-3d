8ff5607a77545888c9378b727829f57a05905f1e
LeoAkira <leohirano95@gmail.com>
2018-12-07 09:11:30 -0200

    Traduzindo codigo alemão

--- snake_motion/test/giro2.py
+++ snake_motion/test/giro2.py
@@ -48,26 +48,26 @@ bus.write_byte_data(address, power_mgmt_1, 0)
 
 while (True):
 
-    gyroskop_xout = read_word_2c(0x43)
-    gyroskop_yout = read_word_2c(0x45)
-    gyroskop_zout = read_word_2c(0x47)
+    gyro_xout = read_word_2c(0x43)
+    gyro_yout = read_word_2c(0x45)
+    gyro_zout = read_word_2c(0x47)
 
-    print( "gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 131))
-    print( "gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 131))
-    print( "gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 131))
+    print( "gyro_xout: ", ("%5d" % gyro_xout), " scaled: ", (gyro_xout / 131))
+    print( "gyro_yout: ", ("%5d" % gyro_yout), " scaled: ", (gyro_yout / 131))
+    print( "gyro_zout: ", ("%5d" % gyro_zout), " scaled: ", (gyro_zout / 131))
 
-    beschleunigung_xout = read_word_2c(0x3b)
-    beschleunigung_yout = read_word_2c(0x3d)
-    beschleunigung_zout = read_word_2c(0x3f)
+    accel_xout = read_word_2c(0x3b)
+    accel_yout = read_word_2c(0x3d)
+    accel_zout = read_word_2c(0x3f)
 
-    beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
-    beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
-    beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
+    accel_xout_scaled = accel_xout / 16384.0
+    accel_yout_scaled = accel_yout / 16384.0
+    accel_zout_scaled = accel_zout / 16384.0
 
-    print( "beschleunigung_xout: ", ("%6d" % beschleunigung_xout), " skaliert: ", beschleunigung_xout_skaliert)
-    print( "beschleunigung_yout: ", ("%6d" % beschleunigung_yout), " skaliert: ", beschleunigung_yout_skaliert)
-    print( "beschleunigung_zout: ", ("%6d" % beschleunigung_zout), " skaliert: ", beschleunigung_zout_skaliert)
+    print( "accel_xout: ", ("%6d" % accel_xout), " scaled: ", accel_xout_scaled)
+    print( "accel_yout: ", ("%6d" % accel_yout), " scaled: ", accel_yout_scaled)
+    print( "accel_zout: ", ("%6d" % accel_zout), " scaled: ", accel_zout_scaled)
 
-    print( "X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
-    print( "Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+    print( "X Rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
+    print( "Y Rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))
     sleep(2)
