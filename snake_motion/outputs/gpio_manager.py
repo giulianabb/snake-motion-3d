2b5b3d424557008f5bbda22e558920435c0cc278
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2018-12-07 11:12:31 -0200

    test

--- snake_motion/outputs/gpio_manager.py
+++ snake_motion/outputs/gpio_manager.py
@@ -42,7 +42,7 @@ class GPIO_Manager:
         ), axis = None)
 
         self.__view[3] = np.concatenate((
-            faces[Face.FRONT][1], np.flip(faces[Face.FRONT][0], 0)
+            np.flip(faces[Face.FRONT][0], faces[Face.FRONT][1], 0)
         ), axis = None)
 
         self.__view[4] = np.concatenate((
@@ -54,7 +54,7 @@ class GPIO_Manager:
         ), axis = None)
 
         self.__view[6] = np.concatenate((
-            faces[Face.FRONT][3], np.flip(faces[Face.FRONT][2], 0)
+            np.flip(faces[Face.FRONT][2], faces[Face.FRONT][3], 0)
         ), axis = None)
 
         self.__view[7] = np.concatenate((
