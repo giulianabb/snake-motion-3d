580325d7fd7581060e80d376aa812059bd8a3e68
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2018-12-07 11:00:13 -0200

    test

--- snake_motion/outputs/gpio_manager.py
+++ snake_motion/outputs/gpio_manager.py
@@ -30,51 +30,51 @@ class GPIO_Manager:
 
     def setFaces(self, faces):
         self.__view[0] = np.concatenate((
-            faces[1][0], np.flip(faces[1][1], 0)
+            faces[Face.TOP][0], np.flip(faces[Face.TOP][1], 0)
         ), axis = None)
 
         self.__view[1] = np.concatenate((
-            faces[1][2], np.flip(faces[1][3], 0)
+            faces[Face.TOP][2], np.flip(faces[Face.TOP][3], 0)
         ), axis = None)
 
         self.__view[2] = np.concatenate((
-            faces[0][0], np.flip(faces[0][1], 0)
+            faces[Face.FRONT][0], np.flip(faces[Face.FRONT][1], 0)
         ), axis = None)
 
         self.__view[3] = np.concatenate((
-            faces[5][2], np.flip(faces[5][3], 0)
+            faces[Face.RIGHT][2], np.flip(faces[Face.RIGHT][3], 0)
         ), axis = None)
 
         self.__view[4] = np.concatenate((
-            faces[4][2], np.flip(faces[4][3], 0)
+            faces[Face.LEFT][2], np.flip(faces[Face.LEFT][3], 0)
         ), axis = None)
 
         self.__view[5] = np.concatenate((
-            faces[2][2], np.flip(faces[2][3], 0)
+            faces[Face.BACK][2], np.flip(faces[Face.BACK][3], 0)
         ), axis = None)
 
         self.__view[6] = np.concatenate((
-            faces[5][1], np.flip(faces[5][0], 0)
+            faces[Face.RIGHT][1], np.flip(faces[Face.RIGHT][0], 0)
         ), axis = None)
 
         self.__view[7] = np.concatenate((
-            faces[4][1], np.flip(faces[4][0], 0)
+            faces[Face.LEFT][1], np.flip(faces[Face.LEFT][0], 0)
         ), axis = None)
 
         self.__view[8] = np.concatenate((
-            faces[2][1], np.flip(faces[2][0], 0)
+            faces[Face.BACK][1], np.flip(faces[Face.BACK][0], 0)
         ), axis = None)
 
         self.__view[9] = np.concatenate((
-            faces[0][3], np.flip(faces[0][2], 0)
+            faces[Face.FRONT][3], np.flip(faces[Face.FRONT][2], 0)
         ), axis = None)
 
         self.__view[10] = np.concatenate((
-            faces[3][0], np.flip(faces[3][1], 0)
+            faces[Face.DOWN][0], np.flip(faces[Face.DOWN][1], 0)
         ), axis = None)
 
         self.__view[11] = np.concatenate((
-            faces[3][3], np.flip(faces[3][2], 0)
+            faces[Face.DOWN][3], np.flip(faces[Face.DOWN][2], 0)
         ), axis = None)
 
         self.showView()
